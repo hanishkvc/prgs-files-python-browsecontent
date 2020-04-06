7d24a1a1df1b0caace476ab8adbc4920d9138a5c
hanishkvc <hanishkvc@gmail.com>
2020-04-06 09:41:27 +0530

    Set size and position, save main window handle to a variable

--- udemykui.py
+++ udemykui.py
@@ -10,6 +10,9 @@ import sys
 
 class UdemyKUI(Gtk.Application):
 	APPNAME = "UdemyKUI"
+	DEFAULT_WIDTH = 640
+	DEFAULT_HEIGHT = 480
+	wMain = None
 
 	def __init__(self):
 		Gtk.Application.__init__(self, application_id="hanishkvc.edu.udemykui")
@@ -18,7 +21,10 @@ class UdemyKUI(Gtk.Application):
 	def do_activate(self):
 		wMain = Gtk.Window(application=self)
 		wMain.set_title(self.APPNAME)
+		wMain.set_default_size(self.DEFAULT_WIDTH, self.DEFAULT_HEIGHT)
+		wMain.set_position(Gtk.WindowPosition.CENTER)
 		wMain.show_all()
+		self.wMain = wMain
 
 
 if __name__ == '__main__':
