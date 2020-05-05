8e30d52e8cf11f9bcb198431e615b91308426a41
hanishkvc <hanishkvc@gmail.com>
2020-05-06 01:45:57 +0530

    List Dirs first followed by files
    The Dirs and Files are inturn stored in class instance variables.

--- udemykui.py
+++ udemykui.py
@@ -33,13 +33,25 @@ class MainWin(Gtk.ApplicationWindow):
 	def on_lb_row_activated(self, listbox, listboxrow):
 		print("listbox: {};\n\t listboxrow: {}".format(listbox, listboxrow))
 
-	def update_lb(self, path=None):
+	def update_lb(self, path=None, mode="all"):
 		if path == None:
 			path = self.curPath
 		dirContents = os.listdir(path)
+		self.curDirList = []
+		self.curFileList = []
 		for cur in dirContents:
-			print(cur)
-			lbl = Gtk.Label(cur)
+			curPath = os.path.join(path, cur)
+			if os.path.isdir(curPath):
+				self.curDirList.append(cur)
+			elif os.path.isfile(curPath):
+				self.curFileList.append(cur)
+			else:
+				continue
+		for cur in self.curDirList:
+			lbl = Gtk.Label("dir:%s"%(cur))
+			self.lbMain.add(lbl)
+		for cur in self.curFileList:
+			lbl = Gtk.Label("file:%s"%(cur))
 			self.lbMain.add(lbl)
 
 
